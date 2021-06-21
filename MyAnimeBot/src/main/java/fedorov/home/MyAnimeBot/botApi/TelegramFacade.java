6dddaeeff3da39c4d7429d99007fbe43b571c79a
DarkReap7 <67169193+DarkReap7@users.noreply.github.com>
2021-06-21 15:50:47 +0300

    !new features
    Added button with url support for /season search.
    Also refactored /season search and added
    year and seasonName check.

--- MyAnimeBot/src/main/java/fedorov/home/MyAnimeBot/botApi/TelegramFacade.java
+++ MyAnimeBot/src/main/java/fedorov/home/MyAnimeBot/botApi/TelegramFacade.java
@@ -1,15 +1,19 @@
 package fedorov.home.MyAnimeBot.botApi;
 
+import com.google.inject.internal.cglib.core.$ClassEmitter;
 import fedorov.home.MyAnimeBot.botApi.handlers.SeasonHandler;
 import fedorov.home.MyAnimeBot.botApi.handlers.TitleHandler;
+import fedorov.home.MyAnimeBot.botApi.util.ButtonHandler;
 import fedorov.home.MyAnimeBot.botApi.util.JsonToAnimeConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 
 import java.io.IOException;
+import java.util.Formatter;
 
 @Component
 public class TelegramFacade {
@@ -17,12 +21,14 @@ public class TelegramFacade {
     private TitleHandler titleHandler;
     private SeasonHandler seasonHandler;
     private JsonToAnimeConverter jsonToAnimeConverter;
+    private ButtonHandler buttonHandler;
 
     @Autowired
-    public TelegramFacade(TitleHandler titleHandler,SeasonHandler seasonHandler,JsonToAnimeConverter jsonToAnimeConverter) {
+    public TelegramFacade(TitleHandler titleHandler,SeasonHandler seasonHandler,JsonToAnimeConverter jsonToAnimeConverter, ButtonHandler buttonHandler) {
         this.titleHandler = titleHandler;
         this.seasonHandler = seasonHandler;
         this.jsonToAnimeConverter = jsonToAnimeConverter;
+        this.buttonHandler = buttonHandler;
     }
 
     public SendMessage handleUpdate(Update update) throws IOException {
@@ -46,14 +52,37 @@ public class TelegramFacade {
             replyMessage = new SendMessage(userId, jsonToAnimeConverter.getAnimeObjectFromJson(titleHandler.getJsonResponseAsString(searchAttribute)).toString());
         }
         if (inputMessage.startsWith("/season")) {
-            String[] splitSearch = inputMessage.split(" ");
-            String year = splitSearch[1];
-            String seasonName = splitSearch[2];
+            seasonHandler.parseInputMessage(inputMessage);
 
-            replyMessage = new SendMessage(userId, seasonHandler.getSeasonUrl(year, seasonName));
+            String year = seasonHandler.getSeasonYear();
+            String seasonName = seasonHandler.getSeasonName();
+
+            replyMessage = checkAndGetInputMessage(userId, year, seasonName);
         }
 
         return replyMessage;
+    }
+
+    private SendMessage checkAndGetInputMessage(long userId, String year, String seasonName) {
+        String seasonUrl;
+        InlineKeyboardMarkup markup;
+        Formatter formatter = new Formatter();
+        SendMessage massageToReturn;
+
+        if (!seasonHandler.checkYear(year) || !seasonHandler.checkSeason(seasonName)) {
+            return new SendMessage(userId, "Sorry, couldn't find anything. " +
+                    "\nPlease use the following search pattern : " +
+                    "\n\"/season year seasonName\"" +
+                    "\n where \"seasonName\" can be: \"winter, spring, summer, fall\" and is not case sensitive.");
+        }
+
+        seasonUrl = seasonHandler.getSeasonUrl(year, seasonName);
+        markup = buttonHandler.getKeyboardMarkup(seasonUrl);
+        String replyString = formatter.format("Here's you link to MAL which contains %s's %s season :", year, seasonName.toLowerCase()).toString();
+
+        massageToReturn = new SendMessage(userId, replyString);
+        massageToReturn.setReplyMarkup(markup);
+        return massageToReturn;
     }
 
 }
